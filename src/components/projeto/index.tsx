a5656d2c83bd3f94cacae6b49e5895e14124de7c
VictorRL <victoraffonso1997@gmail.com>
2024-12-17 17:38:51 -0300

    criando a seção de projetos e o componente de projeto

---
+++ src/components/projeto/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../Titulo'
+import Paragrafo from '../Paragrafo'
+import { Card, LinkBotao } from './styles'
+
+// eslint-disable-next-line
+
+const Projeto = () => (
+  <Card>
+    <Titulo>Projeto Lista de tarefas</Titulo>
+    <Paragrafo tipo="secuntario">Lista de tarefas feitas com VueJS</Paragrafo>
+    <LinkBotao>Visualizar</LinkBotao>
+  </Card>
+)
+
+export default Projeto
